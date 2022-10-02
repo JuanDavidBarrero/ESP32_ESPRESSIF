9a76df7169e8c09ac8835704d61b4c5a2434749a
JuanDavid <Juandbarrero@hotmail.com>
2022-10-02 14:35:06 -0500

    I2c scan added

---
+++ i2cScan/src/main.c
@@ -0,0 +1,45 @@
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+#define I2C_MASTER_SCL_IO 22
+#define I2C_MASTER_SDA_IO 21
+#define I2C_MASTER_FREQ_HZ 100000
+
+static esp_err_t i2c_master_init()
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ};
+
+
+    i2c_param_config(I2C_NUM_0, &conf);
+    return i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
+}
+
+void app_main()
+{
+    // i2c init & scan
+    if (i2c_master_init() != ESP_OK)
+        ESP_LOGE("MAIN", "i2c init failed\n");
+
+     printf("i2c scan: \n");
+     for (uint8_t i = 1; i < 127; i++)
+     {
+        int ret;
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1);
+        i2c_master_stop(cmd);
+        ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_RATE_MS);
+        i2c_cmd_link_delete(cmd);
+    
+        if (ret == ESP_OK)
+        {
+            printf("Found device at: 0x%2x\n", i);
+        }
+    }
+}
