cc312073d04cc42059f5dd993f0976d14d171a1c
JuanDavid <Juandbarrero@hotmail.com>
2022-12-19 09:35:48 -0500

    encrypt and decrypt with rsa

--- esp_timer/src/main.cpp
+++ esp_timer/src/main.cpp
@@ -11,6 +11,8 @@ static const char *TAG = "TIMER";
 static void periodic_timer_callback(void *arg);
 static void oneshot_timer_callback(void *arg);
 
+
+
 extern "C" void app_main()
 {
 
@@ -30,7 +32,6 @@ extern "C" void app_main()
     esp_timer_handle_t oneshot_timer;
     esp_timer_create(&oneshot_timer_args, &oneshot_timer);
 
-
     esp_timer_start_once(oneshot_timer, 5000000);
     esp_timer_start_periodic(periodic_timer, 1000000);
 }
@@ -41,14 +42,14 @@ static void periodic_timer_callback(void *arg)
     ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
 }
 
-static void oneshot_timer_callback(void* timer)
+static void oneshot_timer_callback(void *timer)
 {
     int64_t time_since_boot = esp_timer_get_time();
     ESP_LOGI(TAG, "One-shot timer called, time since boot: %lld us", time_since_boot);
-    esp_timer_handle_t periodic_timer_handle = (esp_timer_handle_t) timer;
+    esp_timer_handle_t periodic_timer_handle = (esp_timer_handle_t)timer;
 
     esp_timer_stop(periodic_timer_handle);
     esp_timer_start_periodic(periodic_timer_handle, 5000000);
     time_since_boot = esp_timer_get_time();
-    ESP_LOGW(TAG, "Restarted periodic timer with 1s period, time since boot: %lld us",time_since_boot);
+    ESP_LOGW(TAG, "Restarted periodic timer with 1s period, time since boot: %lld us", time_since_boot);
 }
